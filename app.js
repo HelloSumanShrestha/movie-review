7666f901d64d6c98eec5bea50c022e0e8f589748
HelloSumanShrestha <iamsuman066@gmail.com>
2024-05-05 14:09:38 +0545

    add, remove, update and sort functionality finalized

--- app.js
+++ app.js
@@ -1,114 +1,150 @@
-import { data } from "./data.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {
+    Firestore,
+    getDocs,
+    getFirestore,
+    onSnapshot,
+    query,
+    collection,
+    orderBy,
+    addDoc,
+    deleteDoc,
+    doc,
+    updateDoc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIsfL2X9grYpB_JwmstsMRxJX-x1Llf6w",
-    authDomain: "book-review-130d7.firebaseapp.com",
-    projectId: "book-review-130d7",
-    storageBucket: "book-review-130d7.appspot.com",
-    messagingSenderId: "135040260798",
-    appId: "1:135040260798:web:0f7d5aeb970ca863565cef",
-    measurementId: "G-B4V4GKTNYZ"
+    apiKey: "AIzaSyAwkyZyBWgk49t2dskPvgQQAar1UhPOBnI",
+    authDomain: "movie-review-aad64.firebaseapp.com",
+    projectId: "movie-review-aad64",
+    storageBucket: "movie-review-aad64.appspot.com",
+    messagingSenderId: "725251961867",
+    appId: "1:725251961867:web:06448fd848ac96ab73830e"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore()
+const db = getFirestore();
 
+const movieCollectionRef = collection(db, "movie-review");
 
-// fetch the data from firestore
+function toggleAddReviewForm() {
+    const form = document.querySelector(".add-movie-review");
+    const header = document.getElementById("add-review-header");
 
-async function fetchMovies() {
-    const movieRef = collection(db, "movie-review")
-    const moviesQuery = query(movieRef)
-    const snapshot = await getDocs(moviesQuery)
-
-    const moviesData = []
-    snapshot.forEach(doc => {
-        moviesData.push(doc.data())
-    })
-
-    return moviesData
+    if (header.innerHTML === "Cancel") {
+        form.classList.add("form-inactive");
+        header.innerHTML = "Add Review";
+    } else {
+        form.classList.remove("form-inactive");
+        header.innerHTML = "Cancel";
+        form.scrollIntoView(true);
+    }
 }
 
-// load the movies review data to the html
+document.getElementById("add-review-header").addEventListener('click', toggleAddReviewForm);
+document.getElementById("cancel-btn").addEventListener('click', toggleAddReviewForm);
 
-const moviesCard = document.getElementById("all-movies");
+function renderMovies(sortValue) {
 
-async function loadMovies() {
+    const sortBy = sortValue || "movieTitle"
 
-    const moviesData = await fetchMovies()
-    console.log(moviesData);
+    const q = query(movieCollectionRef, orderBy(sortBy));
+    onSnapshot(q, (snapshot) => {
 
-    if (moviesData.length > 0) {
-        moviesCard.innerHTML = moviesData.map((movie, index) => {
-            return `
-                <div class="movie" key=${index}>
+        let movies = document.querySelector('#all-movies');
+        movies.innerHTML = "";
+
+        snapshot.forEach(doc => {
+            const movie = doc.data();
+            movies.innerHTML += `
+                <div class="movie">
                     <img src=${movie.movieImage} alt="">
                     <div class="text">
                         <h2>${movie.movieTitle}</h2>
                         <p>${movie.movieDescription}</p>
                         <h3> Director: ${movie.movieDirector}</h3>
                         <h3>Rating: ${movie.movieRating}/5</h3>
+                        <button class="edit" data-id="${doc.id}">Edit</button>
+                        <button class="delete" data-id="${doc.id}">Delete</button>
                     </div>
-                </div>
-            `;
-        }).join('');
-    }
-}
+                </div>`;
+        });
 
-loadMovies();
+        document.querySelectorAll('.delete').forEach(button => {
+            button.addEventListener('click', async (e) => {
+                await deleteDoc(doc(db, 'movie-review', e.target.dataset.id));
+            });
+        });
 
-// add the movie review to the 
-
-async function addMovieReview(event) {
-    event.preventDefault();
-
-    let movieTitle = document.getElementById("movieTitle").value;
-    let movieDescription = document.getElementById("movieDescription").value;
-    let movieDirector = document.getElementById("movieDirector").value;
-    let movieImage = document.getElementById("movieImage").value;
-    let movieRating = document.getElementById("movieRating").value;
-
-    const movieRef = collection(db, "movie-review")
-    const moviesQuery = query(movieRef)
-    const snapshot = await getDocs(moviesQuery)
-    const count = snapshot.size
-
-    await addDoc(movieRef, {
-        movieId: count + 1,
-        movieTitle: movieTitle,
-        movieDescription: movieDescription,
-        movieImage: movieImage,
-        movieDirector: movieDirector,
-        movieRating: movieRating
+        document.querySelectorAll('.edit').forEach(button => {
+            button.addEventListener('click', (e) => {
+                editMovieReview(e.target.dataset.id);
+            });
+        });
     });
-
-    console.log(data);
-
-    document.getElementById("movieTitle").value = "";
-    document.getElementById("movieDescription").value = "";
-    document.getElementById("movieDirector").value = "";
-    document.getElementById("movieImage").value = "";
-    document.getElementById("movieRating").value = "";
-
-    loadMovies();
-    activateForm()
 }
 
-function activateForm() {
-    const form = document.querySelector(".add-movie-review");
+async function editMovieReview(docId) {
+    const docRef = doc(db, 'movie-review', docId);
+    const docSnap = await getDoc(docRef);
 
-    if (document.getElementById("add-review-header").innerHTML === "Cancel") {
-        form.classList.add("form-inactive");
-        document.getElementById("add-review-header").innerHTML = "Add Review";
-    }
-    else {
-        form.classList.remove("form-inactive");
-        document.getElementById("add-review-header").innerHTML = "Cancel";
+    if (docSnap.exists()) {
+        const movie = docSnap.data();
+
+        document.getElementById("movieTitle").value = movie.movieTitle;
+        document.getElementById("movieDescription").value = movie.movieDescription;
+        document.getElementById("movieDirector").value = movie.movieDirector;
+        document.getElementById("movieImage").value = movie.movieImage;
+        document.getElementById("movieRating").value = movie.movieRating;
+        toggleAddReviewForm();
+
+        currentMode = "update";
+        selectedDocId = docId;
+
+        document.getElementById("add-review-btn").textContent = "Update Review";
+
+    } else {
+        console.log("No such document!");
     }
 }
 
-document.getElementById("add-review-btn").addEventListener('click', function () { addMovieReview(event) });
+async function addOrUpdateMovieReview(event) {
+    event.preventDefault();
+    let docRef;
 
-document.getElementById("add-review-header").addEventListener('click', activateForm);
+    const movieData = {
+        movieTitle: document.getElementById("movieTitle").value,
+        movieDescription: document.getElementById("movieDescription").value,
+        movieDirector: document.getElementById("movieDirector").value,
+        movieImage: document.getElementById("movieImage").value,
+        movieRating: +document.getElementById("movieRating").value
+    };
+
+
+    if (currentMode === "add") {
+        docRef = await addDoc(movieCollectionRef, movieData);
+
+    } else if (currentMode === "update" && selectedDocId) {
+        docRef = await updateDoc(doc(db, 'movie-review', selectedDocId), movieData);
+        currentMode = "add";
+        document.getElementById("add-review-btn").textContent = "Add Review";
+    }
+
+    document.querySelectorAll("input").forEach(input => input.value = "");
+    toggleAddReviewForm();
+}
+
+
+// sort by fucntionality
+function sort() {
+    let sortValue = document.getElementById("sort").value;
+    renderMovies(sortValue)
+}
+
+let currentMode = "add";
+let selectedDocId = null;
+
+document.getElementById("add-review-btn").addEventListener('click', addOrUpdateMovieReview);
+document.getElementById("sort").addEventListener('change', sort);
+renderMovies("movieTitle");
